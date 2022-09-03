90980669bfb0f7a69175ebdf0f4ccecf694dfd8d
ajomuch92 <aaronmontes_92@yahoo.es>
2022-09-03 11:34:32 -0600

    feat: 🎸 Initial commit

---
+++ lib/string_2_icon.dart
@@ -0,0 +1,16 @@
+library string_2_icon;
+
+import 'package:flutter/material.dart';
+import './icon_list.dart' show iconDataMap;
+
+String _camelize(String text) {
+  String newString = text.replaceAllMapped(RegExp(r'[-_\s.]+(.)?'), (Match match) => match.group(1)?.toUpperCase() ?? '');
+  return newString.substring(0, 1).toLowerCase() + newString.substring(1);
+}
+
+class String2Icon {
+  static IconData? getIconDataFromString(String iconName) {
+    String iconNameTransformed = _camelize(iconName.replaceAll('mdi', ''));
+    return iconDataMap[iconNameTransformed];
+  }
+}
